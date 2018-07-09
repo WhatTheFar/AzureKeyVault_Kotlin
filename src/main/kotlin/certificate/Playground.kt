94b8cbcafd6e86610d14cee499cadb33c34d869b
BB-8 <ichannel.jakpat@gmail.com>
2018-07-09 15:22:40 +0700

    Added .gitignore

--- src/main/kotlin/certificate/Playground.kt
+++ src/main/kotlin/certificate/Playground.kt
@@ -11,8 +11,21 @@ fun main(args: Array<String>) {
     println("b.toPositiveInt ${b.toPositiveInt()}")
     println("b.toPositiveInt ${b.toPositiveInt().toBinaryString()}")
     println(Integer.toBinaryString(s.toByte().toPositiveInt()))
+
+    test()
 }
 
 fun Byte.toPositiveInt() = toInt() and 0xFF
 
 fun Int.toBinaryString() = Integer.toBinaryString(this)
+
+operator fun <T, R, V> ((T) -> R).rangeTo(other: (R) -> V): ((T) -> V) = { other(this(it)) }
+
+fun same(v: Int) = v
+fun twice(v: Int) = v * 2
+fun trice(v: Int) = v * 3
+
+fun test() {
+    (::same..::twice..::trice..::println)(1)
+    // 6
+}
